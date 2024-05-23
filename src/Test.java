a3b975780ee314e62e6a63791af6544cf49a2a29
Nathan Baines <nathan.baines@nathcat.net>
2024-05-23 15:35:49 +0100

    debugging

--- src/Test.java
+++ src/Test.java
@@ -8,12 +8,7 @@ import java.util.Date;
 public class Test {
     public static void main(String[] args) throws Exception {
         MessageStore msgStore = new MessageStore();
-        MessageQueue q = msgStore.GetMessageQueue(1);
-
-        Message msg;
-        int i = 0;
-        while ((msg = q.Get(i)) != null) {
-            System.out.println(msg); i++;
-        }
+        msgStore.AddMessageQueue(new MessageQueue(1));
+        msgStore.WriteToFile();
     }
 }
