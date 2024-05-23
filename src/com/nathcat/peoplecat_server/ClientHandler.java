f39d3eb9164195ad786e20b035a539e8709469af
Nathan Baines <nathan.baines@nathcat.net>
2024-05-23 15:16:49 +0100

    Debugging issue.

--- src/com/nathcat/peoplecat_server/ClientHandler.java
+++ src/com/nathcat/peoplecat_server/ClientHandler.java
@@ -380,6 +380,7 @@ public class ClientHandler extends ConnectionHandler {
 
                 // Send the response sequence to the client through the output stream
                 for (Packet packet : responseSequence) {
+                    log(String.valueOf(packet.type));
                     if (packet.type != Packet.TYPE_PING) log("Writing packet: \n" + packet + " -> " + packet.getData().toJSONString());
                     else log("Pinging client.");
                     writePacket(packet);
