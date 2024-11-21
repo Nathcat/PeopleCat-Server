c9d551a2df1dd46502cf1b8f0905f87e059213f0
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-11-21 19:32:55 +0000

    Change to version 4.1.3

--- src/com/nathcat/peoplecat_server/Server.java
+++ src/com/nathcat/peoplecat_server/Server.java
@@ -66,7 +66,7 @@ public class Server {
         }
     }
 
-    public static final String version = "4.1.2";
+    public static final String version = "4.1.3";
 
     public int port;
     public int threadCount;
